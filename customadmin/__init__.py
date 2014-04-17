635b7221f0388e62b1d02637788809f68af4c4c4
20tabsrl <20tab@powertab.local>
2014-04-17 18:15:53 +0200

    version changed

--- customadmin/__init__.py
+++ customadmin/__init__.py
@@ -1,6 +1,6 @@
 """
 Customadmin is an application that allow admin interface's customization. 
 """
-VERSION = (0, 6)
+VERSION = (0, 7)
 __version__ = '.'.join(map(str, VERSION))
-DATE = "2014-03-21"
+DATE = "2014-04-17"
