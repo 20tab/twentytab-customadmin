b515889bb5b2ab8804c6028cb1f9d3edc5e0cf51
Rafleze <raffaele@20tab.com>
2014-07-10 15:49:15 +0200

    django 1.7 supported

--- customadmin/__init__.py
+++ customadmin/__init__.py
@@ -1,6 +1,6 @@
 """
 Customadmin is an application that allow admin interface's customization. 
 """
-VERSION = (0, 8)
+VERSION = (0, 9)
 __version__ = '.'.join(map(str, VERSION))
 DATE = "2014-07-10"
