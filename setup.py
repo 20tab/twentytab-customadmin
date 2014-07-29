7777f9830feb70ec3c927182ff1e904084bf66c7
Francesco Leacche <francescoleacche@gmail.com>
2014-07-29 10:23:08 +0200

    now using django-colorful instead of twentytab-colorful

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         'Django >=1.6',
         'Pillow >=2.3',
         'django-imagekit >=3.2',
-        'twentytab-colorful >=0.1',
+        'django-colorful >=1.0.1',
         'twentytab-utils',
         'twentytab-sortable',
         'django-rosetta >=0.7.3',
