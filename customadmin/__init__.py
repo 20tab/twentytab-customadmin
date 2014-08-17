4e13b4b67d79594f07ce90b7587b2b67f4333caf
Rafleze <raffaele@20tab.com>
2014-08-17 15:32:35 +0200

    some css fixed

--- customadmin/__init__.py
+++ customadmin/__init__.py
@@ -1,6 +1,6 @@
 """
 Customadmin is an application that allow admin interface's customization. 
 """
-VERSION = (0, 13)
+VERSION = (0, 14)
 __version__ = '.'.join(map(str, VERSION))
-DATE = "2014-07-15"
+DATE = "2014-08-17"
